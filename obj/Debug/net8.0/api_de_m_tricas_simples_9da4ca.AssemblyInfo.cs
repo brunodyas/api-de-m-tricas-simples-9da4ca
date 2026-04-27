e216d8f624ba0cc45a9657ebc5a7f10faf5da61e
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 14:04:10 -0300

    chore: projeto console .NET 8
    intent: chore

--- obj/Debug/net8.0/api_de_m_tricas_simples_9da4ca.AssemblyInfo.cs
+++ obj/Debug/net8.0/api_de_m_tricas_simples_9da4ca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_de_m_tricas_simples_9da4ca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d078ebc5942d6ede557eabf440f51c1af433e320")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5653130f68d260ebfb87169cd192217e2c670b93")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_de_m_tricas_simples_9da4ca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_de_m_tricas_simples_9da4ca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
